a00cddfe7f3405ac40d53a580a439ad3749ba0c0
Jackster111 <84361338+Jackster111@users.noreply.github.com>
2023-05-15 06:39:38 +1000

    JS chart fixes from 2 weeks ago minus html

---
+++ Data Vis/data_processing/getIDPDataByDate.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+# Read the data from the Excel file
+file_name = "afghan_idp_data.xlsx"
+sheet_name = "14107_Settlements"
+data = pd.read_excel(file_name, sheet_name=sheet_name, engine='openpyxl')
+
+# Select the required columns
+columns = [
+    "ADM1NameEnglish",
+    "ArrivalIDPs2019",
+    "ArrivalIDPs2020",
+    "ArrivalIDPs2021",
+    "ArrivalIDPs2022",
+    "FledIDPs2019",
+    "FledIDPs2020",
+    "FledIDPs2021",
+    "FledIDPs2022",
+]
+selected_data = data[columns]
+
+# Save the selected data as a new CSV file
+output_csv = "idp_19_22.csv"
+selected_data.to_csv(output_csv, index=False)
+
+
--- Data Vis/scripts/script.js
+++ Data Vis/scripts/script.js
@@ -4,8 +4,9 @@ function init() {
 
     //barChart();
     mapVisualization(currentDataType);
-    scatterPlot();
     heatMap();
+
+
 }
 
 function heatMap() {
@@ -90,67 +91,6 @@ function heatMap() {
 
 
 
-function scatterPlot() {
-    const margin = { top: 10, right: 30, bottom: 30, left: 60 };
-    const width = 460 - margin.left - margin.right;
-    const height = 400 - margin.top - margin.bottom;
-
-    const svg = d3
-        .select("body")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", `translate(${margin.left},${margin.top})`);
-
-    const x = d3.scaleLinear().range([0, width]);
-    const y = d3.scaleLinear().range([height, 0]);
-
-    Promise.all([
-        d3.csv("conflict_data.csv", d => {
-            d.Fatalities = +d.Fatalities;
-            return d;
-        }),
-        d3.csv("province_data.csv", d => {
-            d['Arrival IDPs'] = +d['Arrival IDPs'];
-            return d;
-        }),
-    ]).then(([conflictData, idpData]) => {
-        x.domain([0, d3.max(conflictData, d => d.Fatalities)]);
-        y.domain([0, d3.max(idpData, d => d['Arrival IDPs'])]);
-
-        svg
-            .append("g")
-            .attr("transform", `translate(0,${height})`)
-            .call(d3.axisBottom(x));
-
-        svg.append("g").call(d3.axisLeft(y));
-
-        const circles = svg
-            .selectAll("circle")
-            .data(conflictData)
-            .join("circle")
-            .attr("cx", d => x(d.Fatalities))
-            .attr("cy", d => y(idpData[conflictData.indexOf(d)]['Arrival IDPs']))
-            .attr("r", 3)
-            .style("fill", "#69b3a2")
-            .style("opacity", 0.7)
-            .style("stroke", "white");
-
-        circles
-            .on("mouseover", (event, d) => {
-                tooltip.transition().duration(200).style("opacity", 0.9);
-                tooltip
-                    .html(`Fatalities: ${d.Fatalities}<br>IDPs: ${idpData[conflictData.indexOf(d)]['Arrival IDPs']}`)
-                    .style("left", `${event.pageX}px`)
-                    .style("top", `${event.pageY - 28}px`);
-            })
-            .on("mouseout", () => {
-                tooltip.transition().duration(500).style("opacity", 0);
-            });
-    });
-}
-
 
 function mapVisualization(idpType) {
     // Clear the chart before redrawing
@@ -180,14 +120,14 @@ function mapVisualization(idpType) {
             // Define the color scale
             var colorScale = d3.scaleQuantize()
                 .domain([0, d3.max(data, function (d) { return Math.max(d['Arrival IDPs'], d['Fled IDPs']); })])
-            if(idpType == 'Arrival IDPs') {
+            if (idpType == 'Arrival IDPs') {
                 colorScale.range(d3.schemeGreens[6]);
             }
-            else   {
+            else {
                 colorScale.range(d3.schemeReds[6]);
             }
-                    
-                
+
+
 
             // Join the TopoJSON features with the data
             var provinces = topojson.feature(topology, topology.objects.AFGADM2gbOpen).features;
@@ -214,15 +154,9 @@ function mapVisualization(idpType) {
                         .style("left", (event.pageX + 10) + "px")
                         .style("top", (event.pageY - 10) + "px");
                 })
-                .on("click", function(event, d) {
-                    // Draw the bar chart for the hovered province
-                    var provinceData = data.find(function (e) { return e.ADM1NameEnglish === d.properties.shapeName; });
-                    if (provinceData) {
-                        drawBarChart(provinceData);
-                    }
-                    else {
-                        d3.select("#barchart").html("");
-                    }
+                .on("click", function (event, d) {
+                    // Draw the bar chart for the selected province
+                    updateChart(d.properties.shapeName);
                 })
                 .on("mouseout", function () {
                     // Hide tooltip on mouseout
@@ -233,7 +167,28 @@ function mapVisualization(idpType) {
     });
 }
 
-function drawBarChart(provinceData) {
+function updateChart(provinceName) {
+    
+    d3.csv("idp_19_22.csv").then(function (data) {
+        const filteredData = data.find((row) => row.ADM1NameEnglish === provinceName);
+        console.log(row.ADM1NameEnglish);
+        if (filteredData) {
+            
+    
+            const provinceData = {
+                "Arrival IDPs": +filteredData[`ArrivalIDPs${year}`],
+                "Fled IDPs": +filteredData[`FledIDPs${year}`],
+            };
+            console.log(+provinceData["Arrival IDPs"]);
+            console.log(+provinceData["Fled IDPs"]);
+            drawBarChart(provinceData, provinceName, year);
+        } else {
+            drawBarChart(null, provinceName, year);
+        }
+    });
+}
+
+function drawBarChart(provinceData, provinceName, selectedYear) {
     // Remove any existing chart
     d3.select("#barchart").html("");
 
@@ -250,6 +205,26 @@ function drawBarChart(provinceData) {
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+    if (!provinceData) {
+        // Display "No data for this province" if no data is available
+        svg.append("text")
+            .attr("x", width / 2)
+            .attr("y", height / 2)
+            .attr("text-anchor", "middle")
+            .attr("font-size", "14px")
+            .attr("font-weight", "bold")
+            .text("No available data.");
+        return;
+    }
+    // Add heading with province name
+    svg.append("text")
+        .attr("x", width / 2)
+        .attr("y", -margin.top / 2)
+        .attr("text-anchor", "middle")
+        .attr("font-size", "16px")
+        .attr("font-weight", "bold")
+        .text(provinceName);
+
     // Create scales for the bar chart
     var xScale = d3.scaleBand()
         .domain(["Arrival IDPs", "Fled IDPs"])
@@ -257,20 +232,28 @@ function drawBarChart(provinceData) {
         .padding(0.3);
 
     var yScale = d3.scaleLinear()
-        .domain([0, Math.max(provinceData["Arrival IDPs"], provinceData["Fled IDPs"])])
+        .domain([0, Math.max(+provinceData["Arrival IDPs"], +provinceData["Fled IDPs"])])
         .range([height, 0]);
-
+    
     // Draw the bars
     svg.selectAll(".bar")
         .data(["Arrival IDPs", "Fled IDPs"])
         .enter().append("rect")
         .attr("class", "bar")
-        .attr("x", function(d) { return xScale(d); })
-        .attr("y", function(d) { return yScale(provinceData[d]); })
+        .attr("x", function (d) { return xScale(d); })
+        .attr("y", function (d) { return yScale(+provinceData[d + selectedYear]); })
         .attr("width", xScale.bandwidth())
-        .attr("height", function(d) { return height - yScale(provinceData[d]); });
+        .attr("height", function (d) { return height - yScale(+provinceData[d + selectedYear]); })
+        .attr("fill", function (d) {
+            if (d === "Arrival IDPs") {
+                return "green";
+            } else {
+                return "red";
+            }
+        });
 
-    // Add axes
+
+    // Add axis
     var xAxis = d3.axisBottom(xScale);
     var yAxis = d3.axisLeft(yScale);
 
@@ -310,126 +293,24 @@ function onButtonClick() {
 }
 
 
-
-
-
-
+let csvData;
+let currentProvince = "Kabul";
 let currentDataType = "Arrival IDPs";
-window.onload = init;
 
+window.addEventListener("DOMContentLoaded", function () {
+    window.onload = init;
 
+    document.getElementById("year").addEventListener("change", function () {
+        var selectedYear = this.value;
+        var provinceName = currentProvince;
+        drawBarChart(provinceName, selectedYear);
+    });
+    
 
-
-
-
-
-//Bar chart was shit
-
-// function barChart() {
-//     // Set up the chart dimensions and margins
-//     var margin = { top: 20, right: 20, bottom: 30, left: 40 },
-//         width = 960 - margin.left - margin.right,
-//         height = 500 - margin.top - margin.bottom;
-
-//     // Define the x0 scale for provinces
-//     var x0 = d3.scaleBand()
-//         .rangeRound([0, width])
-//         .paddingInner(0.1);
-
-//     // Define the x1 scale for categories (Arrival IDPs and Fled IDPs)
-//     var x1 = d3.scaleBand()
-//         .padding(0.05);
-
-//     // Define the y scale for the number of IDPs
-//     var y = d3.scaleLinear()
-//         .rangeRound([height, 0]);
-
-//     // Define the x and y axes
-//     var xAxis = d3.axisBottom(x0);
-//     var yAxis = d3.axisLeft(y).ticks(null, "s");
-
-//     // Create the main SVG element and a group (g) element inside it with the proper translation
-//     var svg = d3.select("#chart")
-//         .append("svg")
-//         .attr("width", width + margin.left + margin.right)
-//         .attr("height", height + margin.top + margin.bottom)
-//         .append("g")
-//         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-//     // Define the categories (Arrival IDPs and Fled IDPs)
-//     var categories = ['Arrival IDPs', 'Fled IDPs'];
-
-//     // Load and process the data
-//     d3.csv("province_data.csv").then(function (data) {
-//         // Set the domains for the x0, x1, and y scales
-//         x0.domain(data.map(function (d) { return d.ADM1NameEnglish; }));
-//         x1.domain(categories).rangeRound([0, x0.bandwidth()]);
-//         y.domain([0, d3.max(data, function (d) { return Math.max(d['Arrival IDPs'], d['Fled IDPs']); })]);
-
-//         // Add the x axis to the chart
-//         svg.append("g")
-//             .attr("class", "axis")
-//             .attr("transform", "translate(0," + height + ")")
-//             .call(xAxis);
-
-//         // Add the y axis to the chart
-//         svg.append("g")
-//             .attr("class", "axis")
-//             .call(yAxis)
-//             .append("text")
-//             .attr("transform", "rotate(-90)")
-//             .attr("y", 6)
-//             .attr("dy", ".71em")
-//             .style("text-anchor", "end")
-//             .text("IDPs");
-
-//         // Add a group (g) element for each province, positioned according to the x0 scale
-//         var province = svg.selectAll(".province")
-//             .data(data)
-//             .enter().append("g")
-//             .attr("class", "province")
-//             .attr("transform", function (d) { return "translate(" + x0(d.ADM1NameEnglish) + ",0)"; });
-
-//         // Add a rectangle (bar) for each category (Arrival IDPs and Fled IDPs) within each province
-//         province.selectAll("rect")
-//             .data(function (d) { return categories.map(function (key) { return { key: key, value: d[key], province: d.ADM1NameEnglish }; }); })
-//             .enter().append("rect")
-//             .attr("class", "bar")
-//             .attr("x", function (d) { return x1(d.key); })
-//             .attr("y", function (d) { return y(d.value); })
-//             .attr("width", x1.bandwidth())
-//             .attr("height", function (d) { return height - y(d.value); })
-//             .on("mouseover", function (event, d) {
-//                 // Show tooltip on mouseover
-//                 d3.select("#tooltip1")
-//                     .style("opacity", 1)
-//                     .html(d.province + "<br>" + d.key + ": " + d.value)
-//                     .style("left", (event.pageX + 10) + "px")
-//                     .style("top", (event.pageY - 10) + "px");
-//             })
-//             .on("mouseout", function () {
-//                 // Hide tooltip on mouseout
-//                 d3.select("#tooltip1")
-//                     .style("opacity", 0);
-//             });
-
-//         var legend = svg.selectAll(".legend")
-//             .data(categories.slice().reverse())
-//             .enter().append("g")
-//             .attr("class", "legend")
-//             .attr("transform", function (d, i) { return "translate(0," + i * 20 + ")"; });
-
-//         legend.append("rect")
-//             .attr("x", width - 18)
-//             .attr("width", 18)
-//             .attr("height", 18)
-//             .style("fill", function (d, i) { return i === 0 ? 'steelblue' : 'brown'; });
-
-//         legend.append("text")
-//             .attr("x", width - 24)
-//             .attr("y", 9)
-//             .attr("dy", ".35em")
-//             .style("text-anchor", "end")
-//             .text(function (d) { return d; });
-//     });
-// }
+    d3.csv("idp_19_22.csv").then(function (data) {
+        csvData = data;
+        var initialYear = document.getElementById("year").value;
+        var provinceName = currentProvince; 
+        drawBarChart(provinceName, initialYear);
+    });
+});
